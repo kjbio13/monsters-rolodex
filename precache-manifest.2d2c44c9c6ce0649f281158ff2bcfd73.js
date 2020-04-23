96602a0827b6130444658bc933095ca61bfba246
Kevin Jefferson <kevinsouribio13@hotmail.co.uk>
2020-04-23 03:15:13 +0100

    Updates

---
+++ precache-manifest.2d2c44c9c6ce0649f281158ff2bcfd73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e1ff3848ea534804417a1f4e6ed7a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4ebb60329d81a5bdffee",
+    "url": "/monsters-rolodex/static/css/main.62bd4288.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebb60329d81a5bdffee",
+    "url": "/monsters-rolodex/static/js/main.cc066933.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
